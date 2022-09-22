fd35a2051f9278491b14bf6c3f8885ebce60785e
arpitjs <arpited7@gmail.com>
2022-09-22 18:50:04 +0545

    client: img upload in FE

--- src/components/AddNurse.vue
+++ src/components/AddNurse.vue
@@ -1,69 +1,117 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <Header />
-    <h1>Hello User, Welcome to adding Nurse page</h1>
-    <form class="addNurse">
-        <input type="text" name="name" placeholder="Enter Name" v-model="nurse.name" />
-        <input type="text" name="address" placeholder="Enter Address" v-model="nurse.address"/>
-        <input type="text" name="email" placeholder="Enter Email" v-model="nurse.email" />
-        <input type="number" name="phone" placeholder="Enter Phone Number" v-model="nurse.phone" />
+    <div>
+      <Header />
+      <h1>Hello User, Welcome to adding Nurse page</h1>
+  
+      <form class="addNurse">
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          v-model="nurse.name"
+        />
+        <input
+          type="text"
+          name="address"
+          placeholder="Enter Address"
+          v-model="nurse.address"
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter Email"
+          v-model="nurse.email"
+        />
+        <input
+          type="number"
+          name="phone"
+          placeholder="Enter Phone Number"
+          v-model="nurse.phone"
+        />
+  
+        <input
+          type="file"
+          accept="image/*"
+          @change="handleFileChange"
+        />
+  
         <button v-on:click="addNurse" type="button">Add new Nurse</button>
-    
-    </form>
-      
-    </template>
-    <script>
-   
-import Header from './Header.vue'
-    
-    
-    export default {
-        // eslint-disable-next-line vue/multi-word-component-names
-        name:'AddNurse',
-        components:{
-            Header
+      </form>
+  
+    </div>
+  </template>
+  <script>
+  import Header from "./Header.vue";
+  
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "AddNurse",
+    components: {
+      Header,
     },
-    data()
-    {
-        return{
-            nurse:{
-                name:'',
-                address: '',
-                email:'',
-                phone: '',
-            }
+    data() {
+      return {
+        nurse: {
+          name: "",
+          image: "",
+          address: "",
+          email: "",
+          phone: "",
+        },
+      };
+    },
+    methods: {
+  
+      handleFileChange(e) {
+        if (!e.target.files) {
+          return
         }
-    },
-    methods:{
-      async addNurse()
-      {
+        const file = e.target.files[0]
+        // reader
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = (e) => {
+          const imageElement = document.createElement('img')
+          imageElement.src = e.target.result
+          imageElement.onload = (e) => {
+            const canvas = document.createElement('canvas')
+            const MAX_WIDTH = 600
+            const scaleRatio = MAX_WIDTH / e.target.width
+            // canvas size
+            canvas.width = MAX_WIDTH
+            canvas.height = e.target.height * scaleRatio
+            const ctx = canvas.getContext('2d')
+            ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height)
+            const srcEncoded = ctx.canvas.toDataURL(e.target, 'image/jpg')
+            this.nurse.image = srcEncoded;
+          }
+        }
+      },
+  
+      async addNurse() {
         // console.log(this.nurse);
         const res = await fetch("http://localhost:5000/api/nurse", {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json",
-                 Authorization: `Bearer ${localStorage.getItem("token")}`,
-                     },
-       body: JSON.stringify(this.nurse),
-        
-
-      });
-      const result = await res.json();
-      if(result)
-      {
-        this.$router.push({name:'Home'});
-      }
-      console.log("result",result);
-
-    }
-    },
-        mounted()
-      {
-        let user= localStorage.getItem('user-info');
-        if(!user)
-        {
-            this.$router.push({name:'SignUp'});
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify(this.nurse),
+        });
+        const result = await res.json();
+        if (result) {
+          this.$router.push({ name: "Home" });
         }
+        console.log("result", result);
+      },
+    },
+    mounted() {
+      let user = localStorage.getItem("user-info");
+      if (!user) {
+        this.$router.push({ name: "SignUp" });
       }
-    }
-    </script>
+    },
+  };
+  </script>
+  
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,86 +1,94 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <Header />
-  <h1>Hello {{name}}, Welcome to Home page</h1>
-
-  <table border="1">
-    <tr>
-      <td>
-        Name
-      </td>
-      <td>
-        Address
-      </td>
-      <td>
-        Email
-      </td>
-      <td>
-        Phone
-      </td>
-      <td>
-        Actions
-      </td>
-
-    </tr>
-    <tr v-for="item in nurse" :key="item._id">
-
-      <td>{{item.name}}</td>
-      <td>{{item.address}}</td>
-      <td>{{item.email}}</td>
-      <td>{{item.phone}}</td>
-      <td><router-link :to=" '/updateNurse/'+item._id">Update</router-link></td>
-    </tr>
-  </table>
-</template>
-<script>
-// import axios from 'axios';
-import Header from './Header.vue'
-// import axios from 'axios';
-
-export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Home',
-  data() {
-    return {
-
-      name: '',
-      nurse: [],
-
-    }
-  },
-  components: {
-    Header
-  },
-  async mounted() {
-    let user = localStorage.getItem('user-info');
-    this.name = JSON.parse(user).name;
-    if (!user) {
-      this.$router.push({ name: 'SignUp' });
-    }
-    let res = await fetch("http://localhost:5000/api/mynurses", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      }
-    });
-    const result = await res.json();
-    if (!result.error) {
-      console.warn(result.nurses);
-
-
-    } else {
-      console.warn(result);
-    }
-    this.nurse = result.nurses;
-
-  }
-
-}
-</script>
-
-<style>
-  td{
-    width:160px;
-    height:40px;
-  }
-</style>
+  <div>
+   <Header />
+   <h1>Hello {{name}}, Welcome to Home page</h1>
+ 
+   <table border="1">
+     <tr>
+       <td>
+         Image
+       </td>
+       <td>
+         Name
+       </td>
+       <td>
+         Address
+       </td>
+       <td>
+         Email
+       </td>
+       <td>
+         Phone
+       </td>
+       <td>
+         Actions
+       </td>
+ 
+     </tr>
+     <tr v-for="item in nurse" :key="item._id">
+ 
+       <td>
+         <img :src="item.image" alt="no image of this nurse" width="100" height="100"/>
+       </td> 
+       <td>{{item.name}}</td>
+       <td>{{item.address}}</td>
+       <td>{{item.email}}</td>
+       <td>{{item.phone}}</td>
+       <td><router-link :to=" '/updateNurse/'+item._id">Update</router-link></td>
+     </tr>
+   </table>
+  </div>
+ </template>
+ <script>
+ // import axios from 'axios';
+ import Header from './Header.vue'
+ // import axios from 'axios';
+ 
+ export default {
+   // eslint-disable-next-line vue/multi-word-component-names
+   name: 'Home',
+   data() {
+     return {
+ 
+       name: '',
+       nurse: [],
+ 
+     }
+   },
+   components: {
+     Header
+   },
+   async mounted() {
+     let user = localStorage.getItem('user-info');
+     this.name = JSON.parse(user).name;
+     if (!user) {
+       this.$router.push({ name: 'SignUp' });
+     }
+     let res = await fetch("http://localhost:5000/api/mynurses", {
+       method: "GET",
+       headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+       }
+     });
+     const result = await res.json();
+     if (!result.error) {
+       console.warn(result.nurses);
+ 
+ 
+     } else {
+       console.warn(result);
+     }
+     this.nurse = result.nurses;
+ 
+   }
+ 
+ }
+ </script>
+ 
+ <style>
+   td{
+     width:160px;
+     height:40px;
+   }
+ </style>
